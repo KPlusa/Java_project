3e635005594ab446e1f0860cd9c2981d465540a7
KPlusa <48603149+KPlusa@users.noreply.github.com>
2020-05-26 19:53:45 +0200

    Updated

--- src/project/Server.java
+++ src/project/Server.java
@@ -2,14 +2,13 @@ package project;
 
 
 import java.io.*;
+import java.sql.*;
 import java.text.*;
 import java.util.*;
 import java.net.*;
 
 public class Server {
-    private static String url = "jdbc:oracle:thin:@localhost:1521:xe";
-    private static String user = "DB_ADMIN";
-    private static String password = "qazwsx";
+
     public static void main(String[] args) throws IOException {
         ServerSocket ss = new ServerSocket(5057);
         System.out.println("Server dziala");
@@ -42,8 +41,14 @@ class ClientHandler extends Thread {
     final DataInputStream dis;
     final DataOutputStream dos;
     final Socket s;
-    public String username = "admin";
-    public String password = "admin";
+    private String username;
+    private String password;// = "admin";
+    private static String url = "jdbc:oracle:thin:@localhost:1521:xe";
+    private static String db_user = "DB_ADMIN";
+    private static String db_pass = "qazwsx";
+    private static Connection con;
+    private Statement stmt;
+    private ResultSet rs;
 
     public ClientHandler(Socket s, DataInputStream dis, DataOutputStream dos) {
         this.s = s;
@@ -53,6 +58,14 @@ class ClientHandler extends Thread {
 
     @Override
     public void run() {
+        try {
+
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            con = DriverManager.getConnection(
+                    url, db_user, db_pass);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
         while (true) {
 
@@ -60,14 +73,24 @@ class ClientHandler extends Thread {
                 int choice = dis.readInt();
                 switch (choice) {
                     case 1:
+                        stmt = con.createStatement();
+                        rs = stmt.executeQuery("select nazwa,haslo from Uzytkownik");
                         System.out.println("Logowanie");
                         String to_return = "Niepoprawna nazwa uzytkownika lub haslo";
                         String login = dis.readUTF();
                         String pass = dis.readUTF();
                         System.out.println("Wprowadzany login: " + login + "\n" + "Wprowadzane haslo: " + pass);
-
-                        if (login.equals(username) && pass.equals(password))
-                            to_return = "Poprawne dane";
+                        while (rs.next()) {
+                            username =rs.getString(1);
+                            password =rs.getString(2);
+                            if (login.equals(username) && pass.equals(password)) {
+                                to_return = "Poprawne dane";
+                                break;
+                            }
+                        }
+                        con.close();
+                        //if (login.equals(username) && pass.equals(password))
+                           //to_return = "Poprawne dane";
 
                         dos.writeUTF(to_return);
                         //System.out.println("Zamykanie socketa");
@@ -78,7 +101,7 @@ class ClientHandler extends Thread {
                 }
 
 break;
-            } catch (IOException e) {
+            } catch (IOException | SQLException e) {
                 e.printStackTrace();
             }
         }
