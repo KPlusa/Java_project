3cc19013bab86af216f102ff23c660452ef87bfc
Rafał <rafal.robak@interia.pl>
2020-06-12 16:33:18 +0200

    12.06.2020

--- src/project/EditQC.java
+++ src/project/EditQC.java
@@ -1,74 +1,69 @@
 package project;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
+
 
 public class EditQC {
     private int id;
     private String pytanie;
-    private String  odp_a;
-    private String  odp_b;
-    private String  odp_c;
-    private String  odp_d;
+    private String odp_a;
+    private String odp_b;
+    private String odp_c;
+    private String odp_d;
 
     public EditQC(){
+        this.id= 0;
+        this.pytanie= "";
+        this.odp_a="";
+        this.odp_b="";
+        this.odp_c="";
+        this.odp_d="";
     }
-
-
-    public int getId(){
+    public int get_id(){
         return  id;
     }
-
-    public void setId(int id)
+    public void set_id(int id)
     {
         this.id=id;
     }
     ///////////////////////////////
-    public String getPytanie(){ return  pytanie;
+    public String get_pytanie(){ return  pytanie;
     }
-
-    public void setPytanie(String pytanie)
+    public void set_pytanie(String pytanie)
     {
         this.pytanie=pytanie;
     }
     //////////////////////////////////
-    public String getOdp_a(){
+    public String get_odp_a(){
         return  odp_a;
     }
-
-    public void setOdp_a(String odp_a)
+    public void set_Podp_a(String odp_a)
     {
         this.odp_a=odp_a;
     }
     //////////////////////////////////
 
-    public String getOdp_b(){
+    public String get_odp_b(){
         return  odp_b;
     }
-
-    public void setOdp_b(String odp_b)
+    public void set_Podp_b(String odp_b)
     {
         this.odp_b=odp_b;
     }
     //////////////////////////////////
 
-    public String getOdp_c(){
+    public String get_odp_c(){
         return  odp_c;
     }
-
-    public void setOdp_c(String odp_c)
+    public void set_Podp_c(String odp_c)
     {
         this.odp_c=odp_c;
     }
     //////////////////////////////////
 
-    public String getOdp_d(){
+    public String get_odp_d(){
         return  odp_d;
     }
-
-    public void setOdp_d(String odp_d)
+    public void set_Podp_d(String odp_d)
     {
         this.odp_d=odp_d;
     }
--- src/project/controllers/QuestionsController.java
+++ src/project/controllers/QuestionsController.java
@@ -1,5 +1,7 @@
 package project.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,37 +9,66 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import project.Questionsclass;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class QuestionsController implements Initializable {
     private double x, y;
+    private String st;
+    private int counter;
+    private Socket s;
+    private Integer id;
+    private String przedmiot, typ, tresc;
+    private InetAddress ip;
+    private DataInputStream dis;
+    private DataOutputStream dos;
     private Stage stage;
     @FXML
     private AnchorPane AnchorPaneMain;
+
     @FXML
-    private void closeAction(MouseEvent event){
+    private void closeAction(MouseEvent event) {
         System.exit(0);
     }
 
     @FXML
-    private void minAction(MouseEvent event){
-        Stage stage=(Stage) AnchorPaneMain.getScene().getWindow();
+    TableView<Questionsclass> table;
+    @FXML
+    private TableColumn<Questionsclass, Integer> col_id;
+    @FXML
+    private TableColumn<Questionsclass, String> col_przedmiot;
+    @FXML
+    private TableColumn<Questionsclass, String> col_typ;
+    @FXML
+    private TableColumn<Questionsclass, String> col_tresc;
+
+
+    @FXML
+    private void minAction(MouseEvent event) {
+        Stage stage = (Stage) AnchorPaneMain.getScene().getWindow();
         stage.setIconified(true);
     }
+
     @FXML
-    private void maxAction(MouseEvent event){
-        Stage stage=(Stage) AnchorPaneMain.getScene().getWindow();
-        if(stage.isMaximized()) {
+    private void maxAction(MouseEvent event) {
+        Stage stage = (Stage) AnchorPaneMain.getScene().getWindow();
+        if (stage.isMaximized()) {
             stage.setMaximized(false);
             stage.setResizable(false);
-        }
-        else {
+        } else {
             stage.setMaximized(true);
             stage.setResizable(true);
         }
@@ -47,6 +78,18 @@ public class QuestionsController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         makeDraggable();
+        col_id.setMinWidth(2);
+        col_przedmiot.setMinWidth(200);
+        col_tresc.setMinWidth(200);
+        col_typ.setMinWidth(200);
+        col_id.setCellValueFactory(new PropertyValueFactory<>("id"));
+        col_przedmiot.setCellValueFactory(new PropertyValueFactory<>("przedmiot"));
+        col_typ.setCellValueFactory(new PropertyValueFactory<>("typ"));
+        col_tresc.setCellValueFactory(new PropertyValueFactory<>("tresc"));
+        try {
+            table.setItems(fill_table());
+        } catch (Exception e) {
+        }
 
     }
 
@@ -59,6 +102,7 @@ public class QuestionsController implements Initializable {
         window.setScene(scene);
         window.show();
     }
+
     @FXML
     public void go_menu_avatar(MouseEvent event) throws IOException {
         Parent parent = FXMLLoader.load(getClass().getResource("../fxml/menu.fxml"));
@@ -67,20 +111,58 @@ public class QuestionsController implements Initializable {
         window.setScene(scene);
         window.show();
     }
+
     @FXML
-    private void makeDraggable()
-    {
+    private void makeDraggable() {
         AnchorPaneMain.setOnMousePressed(((event) -> {
-            x=event.getSceneX();
-            y=event.getSceneY();
+            x = event.getSceneX();
+            y = event.getSceneY();
         }));
 
         AnchorPaneMain.setOnMouseDragged(((event) -> {
-            stage= (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setX(event.getScreenX()-x);
-            stage.setY(event.getScreenY()-y);
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
         }));
     }
 
+
+    @FXML
+    private ObservableList<Questionsclass> fill_table() throws IOException {
+        ObservableList<Questionsclass> questions = FXCollections.observableArrayList();
+        try {
+            while (true) {
+                try {
+                    ip = InetAddress.getByName("localhost");
+                    s = new Socket(ip, 5057);
+                    dis = new DataInputStream(s.getInputStream());
+                    dos = new DataOutputStream(s.getOutputStream());
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                dos.writeInt(8);
+                counter = dis.readInt();
+                for (int i = 1; i <= counter; i++) {
+                    id=i;
+                    przedmiot = dis.readUTF();
+                    System.out.println(przedmiot);
+                    typ = dis.readUTF();
+                    System.out.println(typ);
+                    tresc = dis.readUTF();
+                    System.out.println(tresc);
+                    questions.add(new Questionsclass(id, przedmiot, typ, tresc));
+                }
+
+                break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            dis.close();
+            dos.close();
+            s.close();
+        }
+        return questions;
+    }
 }
 
