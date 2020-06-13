1d0272369cdb41172ece10bcea40bdb055576fe5
KPlusa <48603149+KPlusa@users.noreply.github.com>
2020-06-13 14:44:28 +0200

    Updated

--- src/project/controllers/RankController.java
+++ src/project/controllers/RankController.java
@@ -1,7 +1,5 @@
 package project.controllers;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,75 +7,38 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import project.Materialsclass;
-import project.Rankclass;
 import project.StoreLogin;
 
-import javax.swing.*;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class RankController extends StoreLogin implements Initializable {
     private double x, y;
     private Stage stage;
-    private int counter;
-    private Socket s;
-    private String name;
-    private String subject;
-    private String sub;
-    private InetAddress ip;
-    private DataInputStream dis;
-    private DataOutputStream dos;
-    int id, points;
-    final ObservableList mylist = FXCollections.observableArrayList();
     @FXML
     private AnchorPane AnchorPaneMain;
     @FXML
-    private ComboBox chb;
-    @FXML
-    TableView<Rankclass> table;
-    @FXML
-    private TableColumn<Rankclass, Integer> ColId;
-    @FXML
-    private TableColumn<Rankclass, String> ColSub;
-    @FXML
-    private TableColumn<Rankclass, String> ColName;
-    @FXML
-    private TableColumn<Rankclass, Integer> ColPoints;
-    @FXML
-    private TextField text;
-
-    @FXML
-    private void closeAction(MouseEvent event) {
+    private void closeAction(MouseEvent event){
         System.exit(0);
     }
 
     @FXML
-    private void minAction(MouseEvent event) {
-        Stage stage = (Stage) AnchorPaneMain.getScene().getWindow();
+    private void minAction(MouseEvent event){
+        Stage stage=(Stage) AnchorPaneMain.getScene().getWindow();
         stage.setIconified(true);
     }
-
     @FXML
-    private void maxAction(MouseEvent event) {
-        Stage stage = (Stage) AnchorPaneMain.getScene().getWindow();
-        if (stage.isMaximized()) {
+    private void maxAction(MouseEvent event){
+        Stage stage=(Stage) AnchorPaneMain.getScene().getWindow();
+        if(stage.isMaximized()) {
             stage.setMaximized(false);
             stage.setResizable(false);
-        } else {
+        }
+        else {
             stage.setMaximized(true);
             stage.setResizable(true);
         }
@@ -87,20 +48,6 @@ public class RankController extends StoreLogin implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         makeDraggable();
-
-        ColId.setCellValueFactory(new PropertyValueFactory<>("id"));
-        ColSub.setCellValueFactory(new PropertyValueFactory<>("subject"));
-        ColName.setCellValueFactory(new PropertyValueFactory<>("name"));
-        ColPoints.setCellValueFactory(new PropertyValueFactory<>("points"));
-        try {
-            table.setItems(fill_table());
-        } catch (Exception e) {
-        }
-        try {
-            fillcombo();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
     }
 
@@ -116,7 +63,6 @@ public class RankController extends StoreLogin implements Initializable {
         window.setScene(scene);
         window.show();
     }
-
     @FXML
     public void go_menu_avatar(MouseEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../fxml/menu.fxml"));
@@ -128,162 +74,20 @@ public class RankController extends StoreLogin implements Initializable {
         window.setScene(scene);
         window.show();
     }
-
     @FXML
-    private void makeDraggable() {
+    private void makeDraggable()
+    {
         AnchorPaneMain.setOnMousePressed(((event) -> {
-            x = event.getSceneX();
-            y = event.getSceneY();
+            x=event.getSceneX();
+            y=event.getSceneY();
         }));
 
         AnchorPaneMain.setOnMouseDragged(((event) -> {
-            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setX(event.getScreenX() - x);
-            stage.setY(event.getScreenY() - y);
+            stage= (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setX(event.getScreenX()-x);
+            stage.setY(event.getScreenY()-y);
         }));
     }
 
-    @FXML
-    private ObservableList<Rankclass> fill_table() throws IOException {
-        ObservableList<Rankclass> rank = FXCollections.observableArrayList();
-        try {
-            while (true) {
-                try {
-                    ip = InetAddress.getByName("localhost");
-                    s = new Socket(ip, 5057);
-                    dis = new DataInputStream(s.getInputStream());
-                    dos = new DataOutputStream(s.getOutputStream());
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                dos.writeInt(10);
-                dos.writeUTF("");
-                counter = dis.readInt();
-                for (int i = 0; i < counter; i++) {
-                    id = dis.readInt();
-                    name = dis.readUTF();
-                    subject = dis.readUTF();
-                    points = dis.readInt();
-                    rank.add(new Rankclass(id, subject, name, points));
-                }
-
-                break;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            dis.close();
-            dos.close();
-            s.close();
-        }
-        return rank;
-    }
-
-
-    @FXML
-    private void fillcombo() throws IOException {
-        chb.setMaxHeight(30);
-        mylist.clear();
-        try {
-            while (true) {
-                try {
-                    ip = InetAddress.getByName("localhost");
-                    s = new Socket(ip, 5057);
-                    dis = new DataInputStream(s.getInputStream());
-                    dos = new DataOutputStream(s.getOutputStream());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    System.out.println("Brak polaczenia z serwerem");
-                }
-                dos.writeInt(3);
-                counter = dis.readInt();
-                for (int i = 0; i < counter; i++) {
-                    sub = dis.readUTF();
-                    mylist.add(sub);
-                }
-                chb.setItems(mylist);
-                break;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            dis.close();
-            dos.close();
-            s.close();
-        }
-
-    }
-    @FXML
-    private void setdisplay() throws IOException{
-        table.setItems(Combo_fill_table());
-    }
-    @FXML
-    private ObservableList<Rankclass> Combo_fill_table() throws IOException {
-        ObservableList<Rankclass> rank = FXCollections.observableArrayList();
-        table.getItems().clear();
-        sub = chb.getValue().toString();
-        text.clear();
-        try {
-            while (true) {
-                counter = 0;
-                ip = InetAddress.getByName("localhost");
-                s = new Socket(ip, 5057);
-                dis = new DataInputStream(s.getInputStream());
-                dos = new DataOutputStream(s.getOutputStream());
-                dos.writeInt(11);
-                dos.writeUTF(sub);
-                counter = dis.readInt();
-                for (int i = 0; i < counter; i++) {
-                    id = dis.readInt();
-                    name = dis.readUTF();
-                    subject = dis.readUTF();
-                    points = dis.readInt();
-                    rank.add(new Rankclass(id, subject, name, points));
-                }
-                break;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            dis.close();
-            dos.close();
-            s.close();
-        }
-        return rank;
-    }
-    @FXML
-    private void setdisplaybyname() throws IOException{
-        table.setItems(Combo_fill_table_by_name());
-    }
-    @FXML
-    private ObservableList<Rankclass> Combo_fill_table_by_name() throws IOException {
-        ObservableList<Rankclass> rank = FXCollections.observableArrayList();
-        table.getItems().clear();
-        sub = text.getText();
-        chb.setValue("");
-        try {
-            while (true) {
-                counter = 0;
-                ip = InetAddress.getByName("localhost");
-                s = new Socket(ip, 5057);
-                dis = new DataInputStream(s.getInputStream());
-                dos = new DataOutputStream(s.getOutputStream());
-                dos.writeInt(12);
-                dos.writeUTF(sub);
-                counter = dis.readInt();
-                for (int i = 0; i < counter; i++) {
-                    id = dis.readInt();
-                    name = dis.readUTF();
-                    subject = dis.readUTF();
-                    points = dis.readInt();
-                    rank.add(new Rankclass(id, subject, name, points));
-                }
-                break;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            dis.close();
-            dos.close();
-            s.close();
-        }
-        return rank;
-    }
 }
+
--- src/project/controllers/Test_openedController.java
+++ src/project/controllers/Test_openedController.java
@@ -1,7 +1,5 @@
 package project.controllers;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,53 +7,20 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import project.StoreLogin;
-import project.Testclosedclass;
-import project.Testopenclass;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.Socket;
 import java.net.URL;
-import java.util.Random;
 import java.util.ResourceBundle;
 
 public class Test_openedController extends StoreLogin implements Initializable {
     private double x,y;
-    private int counter,id,obecne;
-    private Socket s;
-    private InetAddress ip;
-    private DataInputStream dis;
-    private DataOutputStream dos;
     private Stage stage;
-    private String subject,type,tresc,odp_o,ans;
-    final ObservableList<Testopenclass> Testopen = FXCollections.observableArrayList();
-    Random generator = new Random();
-    private int[] ilosc=new int[20];
-    private int[] pkt={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    private String obecna_poprawna;
-    private int suma_punktow=0;
-    private String[] was_selected={"","","","","","","","","","","","","","","","","","","",""};
     @FXML
     private AnchorPane AnchorPaneMain;
-    @FXML
-    private Label nr_pyt;
-    @FXML
-    private Label question;
-    @FXML
-    private Label error_msg;
-    @FXML
-    private TextArea odpowiedz;
-
-
     @FXML
     private void closeAction(MouseEvent event){
         System.exit(0);
@@ -122,156 +87,6 @@ public class Test_openedController extends StoreLogin implements Initializable {
             stage.setX(event.getScreenX()-x);
             stage.setY(event.getScreenY()-y);
         }));
-    }
-    public ObservableList<Testopenclass> fill_test(String sub, String typ) throws IOException {
-        try {
-            ip = InetAddress.getByName("localhost");
-            s = new Socket(ip, 5057);
-            dis = new DataInputStream(s.getInputStream());
-            dos = new DataOutputStream(s.getOutputStream());
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Brak polaczenia z serwerem");
-        }
-        subject=sub;
-        type=typ;
-        dos.writeInt(33);
-        dos.writeUTF(sub);
-        dos.writeUTF(typ);
-        counter=dis.readInt();
-        for(int i=0;i<counter;i++)
-        {
-            id=dis.readInt();
-            id++;
-            tresc = dis.readUTF();
-            odp_o = dis.readUTF();
-            Testopen.add(new Testopenclass(id,tresc,odp_o));
-        }
-
-        return Testopen;
-    }
-    public void gen_number()throws IOException {
-        for (int i = 0; i < 20; i++) {
-            int num = generator.nextInt(counter) + 1;
-            ilosc[i]=num;
-            for (int j = 0; j <= i; j++) {
-                if(i==0)
-                {
-                    break;
-                }
-                else if (ilosc[j] == num&&j!=i ) {
-                    i--;
-                    break;
-                }
-            }
-
-        }
-    }
-    @FXML
-    public void fill_first_quest() throws IOException
-    {
-        nr_pyt.setText("1");
-        for(Testopenclass cl:Testopen)
-        {
-            if(cl.getId()==ilosc[0])
-            {
-                question.setText(cl.getTresc());
-                obecna_poprawna=cl.getOdp_o();
-            }
-        }
-    }
-    @FXML
-    private void go_right(MouseEvent event) throws IOException
-    {
-        ans=odpowiedz.getText();
-        System.out.println(ans);
-        was_selected[obecne]=ans;
-        if(obecna_poprawna.equals(ans)) {
-            pkt[obecne]=1;
-        }
-        else {
-            pkt[obecne]=0;
-        }
-
-        if(obecne==19)
-        {
-            error_msg.setText("To ostatnie pytanie");
-
-        }
-        else {
-            if(obecne!=19){obecne++;}
-            for (Testopenclass cl : Testopen) {
-                if (cl.getId() == ilosc[obecne]) {
-                    error_msg.setText("");
-                    question.setText(cl.getTresc());
-                    obecna_poprawna = cl.getOdp_o();
-                }
-
-            }
-        }
-        odpowiedz.setText(was_selected[obecne]);
-        nr_pyt.setText(String.valueOf(obecne+1));
-    }
-    @FXML
-    private void go_left(MouseEvent event) throws IOException
-    {
-        ans=odpowiedz.getText();
-        System.out.println(ans);
-        was_selected[obecne]=ans;
-        if(obecna_poprawna.equals(ans)) {
-            pkt[obecne]=1;
-        }
-        else {
-            pkt[obecne]=0;
-        }
-
-        if(obecne==0)
-        {
-            error_msg.setText("To pierwsze pytanie");
-        }
-        else {
-            if(obecne!=0) {
-                obecne--;
-            }
-            for (Testopenclass cl : Testopen) {
-
-                if (cl.getId() == ilosc[obecne]) {
-                    error_msg.setText("");
-                    question.setText(cl.getTresc());
-                    obecna_poprawna = cl.getOdp_o();
-                }
-
-            }
-        }
-        odpowiedz.setText(was_selected[obecne]);
-        nr_pyt.setText(String.valueOf(obecne+1));
-    }
-    @FXML
-    private void end_test(MouseEvent event) throws IOException
-    {
-        ans=odpowiedz.getText();
-        System.out.println(ans);
-        if(obecna_poprawna.equals(ans)) {
-            pkt[obecne]=1;
-        }
-        else {
-            pkt[obecne]=0;
-        }
-
-        for(int i=0;i<20;i++)
-        {
-            suma_punktow=suma_punktow+pkt[i];
-        }
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("../fxml/Show_score.fxml"));
-        Parent root = loader.load();
-        Show_scoreController showController = loader.getController();
-        showController.store_username(login);
-        showController.pkt(suma_punktow,subject,type);
-        Scene scene = new Scene(root);
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window.setScene(scene);
-        window.show();
-
     }
 
 }
