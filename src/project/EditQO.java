8fa47c3e07e33d15d3c991ace01858c617f6f42b
Rafał <rafal.robak@interia.pl>
2020-06-16 18:42:55 +0200

    16.06.2020-end

---
+++ src/project/EditQO.java
@@ -0,0 +1,37 @@
+package project;
+
+public class EditQO {
+    private int id;
+    private String pytanie, odp;
+    public EditQO(){
+        this.id= 0;
+        this.pytanie= "";
+        this.odp="";
+
+    }
+    public EditQO(int id, String pytanie, String odp){
+        this.id= id;
+        this.pytanie= pytanie;
+        this.odp=odp;
+
+    }
+    public int getId(){
+        return  this.id;
+    }
+    public void setId(int id)
+    {
+        this.id=id;
+    }
+    public String getPytanie(){ return  this.pytanie;}
+    public void setPytanie(String pytanie)
+    {
+        this.pytanie=pytanie;
+    }
+    public String getOdp(){
+        return  this.odp;
+    }
+    public void setOdp(String odp_a)
+    {
+        this.odp=odp;
+    }
+}
